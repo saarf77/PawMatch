c11e92b0f9757057132b3e51b31a2684bb2ba577
saarfridman <saarf77@gmail.com>
2026-03-23 17:57:34 +0100

    Bundle car logos as local SVG assets
    Download 25 real car brand SVGs (simpleicons.org).
    Configure metro.config.js with react-native-svg-transformer to load .svg files.
    Cars component now uses bundled assets - works offline, no CDN needed.

--- src/components/cars/index.tsx
+++ src/components/cars/index.tsx
@@ -1,82 +1,73 @@
 import React from "react"
 import { Image, View } from "react-native"
 
-// Uses car brand logos from a public CDN (car-logos.org)
-const BASE = "https://www.carlogos.org/car-logos/"
-
-const CAR_LOGO_URLS: Record<string, string> = {
-  bmw:          "https://www.carlogos.org/car-logos/bmw-logo-2020-gray-download.png",
-  mercedes:     "https://www.carlogos.org/car-logos/mercedes-benz-logo-2011-download.png",
-  ferrari:      "https://www.carlogos.org/car-logos/ferrari-logo-750x1100.png",
-  audi:         "https://www.carlogos.org/car-logos/audi-logo-2016-download.png",
-  toyota:       "https://www.carlogos.org/car-logos/toyota-logo-2020-europe-download.png",
-  volkswagen:   "https://www.carlogos.org/car-logos/volkswagen-logo-2019-download.png",
-  porsche:      "https://www.carlogos.org/car-logos/porsche-logo-2014-download.png",
-  lamborghini:  "https://www.carlogos.org/car-logos/lamborghini-logo-download.png",
-  ford:         "https://www.carlogos.org/car-logos/ford-logo-2017-download.png",
-  chevrolet:    "https://www.carlogos.org/car-logos/chevrolet-logo-2013-download.png",
-  honda:        "https://www.carlogos.org/car-logos/honda-logo-download.png",
-  nissan:       "https://www.carlogos.org/car-logos/nissan-logo-2020-black-download.png",
-  hyundai:      "https://www.carlogos.org/car-logos/hyundai-logo-2011-download.png",
-  kia:          "https://www.carlogos.org/car-logos/kia-logo-2021-download.png",
-  mazda:        "https://www.carlogos.org/car-logos/mazda-logo-2018-download.png",
-  subaru:       "https://www.carlogos.org/car-logos/subaru-logo-2019-download.png",
-  rollsroyce:   "https://www.carlogos.org/car-logos/rolls-royce-logo-download.png",
-  bentley:      "https://www.carlogos.org/car-logos/bentley-logo-2002-download.png",
-  maserati:     "https://www.carlogos.org/car-logos/maserati-logo-download.png",
-  jaguar:       "https://www.carlogos.org/car-logos/jaguar-logo-2012-download.png",
-  volvo:        "https://www.carlogos.org/car-logos/volvo-logo-2014-download.png",
-  peugeot:      "https://www.carlogos.org/car-logos/peugeot-logo-2021-download.png",
-  renault:      "https://www.carlogos.org/car-logos/renault-logo-2021-download.png",
-  tesla:        "https://www.carlogos.org/car-logos/tesla-logo-2007-download.png",
-  bugatti:      "https://www.carlogos.org/car-logos/bugatti-logo-download.png",
+const logos: Record<string, any> = {
+  bmw:         require("../../../assets/cars/bmw.svg"),
+  mercedes:    require("../../../assets/cars/mercedes.svg"),
+  ferrari:     require("../../../assets/cars/ferrari.svg"),
+  audi:        require("../../../assets/cars/audi.svg"),
+  toyota:      require("../../../assets/cars/toyota.svg"),
+  volkswagen:  require("../../../assets/cars/volkswagen.svg"),
+  porsche:     require("../../../assets/cars/porsche.svg"),
+  lamborghini: require("../../../assets/cars/lamborghini.svg"),
+  ford:        require("../../../assets/cars/ford.svg"),
+  chevrolet:   require("../../../assets/cars/chevrolet.svg"),
+  honda:       require("../../../assets/cars/honda.svg"),
+  nissan:      require("../../../assets/cars/nissan.svg"),
+  hyundai:     require("../../../assets/cars/hyundai.svg"),
+  kia:         require("../../../assets/cars/kia.svg"),
+  mazda:       require("../../../assets/cars/mazda.svg"),
+  subaru:      require("../../../assets/cars/subaru.svg"),
+  rollsroyce:  require("../../../assets/cars/rollsroyce.svg"),
+  bentley:     require("../../../assets/cars/bentley.svg"),
+  maserati:    require("../../../assets/cars/maserati.svg"),
+  jaguar:      require("../../../assets/cars/jaguar.svg"),
+  volvo:       require("../../../assets/cars/volvo.svg"),
+  peugeot:     require("../../../assets/cars/peugeot.svg"),
+  renault:     require("../../../assets/cars/renault.svg"),
+  tesla:       require("../../../assets/cars/tesla.svg"),
+  bugatti:     require("../../../assets/cars/bugatti.svg"),
 }
 
-function CarLogo({ carId, size = 80 }: { carId: string; size?: number }) {
-  const url = CAR_LOGO_URLS[carId]
-  return (
-    <View style={{ width: size, height: size, justifyContent: "center", alignItems: "center", backgroundColor: "white", borderRadius: 8 }}>
-      <Image
-        source={{ uri: url }}
-        style={{ width: size * 0.85, height: size * 0.85 }}
-        resizeMode="contain"
-      />
-    </View>
-  )
-}
-
-// Generate a component for each car
 function makeCarComponent(carId: string): React.ComponentType<{ size?: number }> {
   return function CarComponent({ size = 80 }: { size?: number }) {
-    return <CarLogo carId={carId} size={size} />
+    return (
+      <View style={{ width: size, height: size, justifyContent: "center", alignItems: "center", backgroundColor: "white", borderRadius: 8, padding: 6 }}>
+        <Image
+          source={logos[carId]}
+          style={{ width: size * 0.82, height: size * 0.82 }}
+          resizeMode="contain"
+        />
+      </View>
+    )
   }
 }
 
-export const BMW          = makeCarComponent("bmw")
-export const Mercedes     = makeCarComponent("mercedes")
-export const Ferrari      = makeCarComponent("ferrari")
-export const Audi         = makeCarComponent("audi")
-export const Toyota       = makeCarComponent("toyota")
-export const Volkswagen   = makeCarComponent("volkswagen")
-export const Porsche      = makeCarComponent("porsche")
-export const Lamborghini  = makeCarComponent("lamborghini")
-export const Ford         = makeCarComponent("ford")
-export const Chevrolet    = makeCarComponent("chevrolet")
-export const Honda        = makeCarComponent("honda")
-export const Nissan       = makeCarComponent("nissan")
-export const Hyundai      = makeCarComponent("hyundai")
-export const Kia          = makeCarComponent("kia")
-export const Mazda        = makeCarComponent("mazda")
-export const Subaru       = makeCarComponent("subaru")
-export const RollsRoyce   = makeCarComponent("rollsroyce")
-export const Bentley      = makeCarComponent("bentley")
-export const Maserati     = makeCarComponent("maserati")
-export const Jaguar       = makeCarComponent("jaguar")
-export const Volvo        = makeCarComponent("volvo")
-export const Peugeot      = makeCarComponent("peugeot")
-export const Renault      = makeCarComponent("renault")
-export const Tesla        = makeCarComponent("tesla")
-export const Bugatti      = makeCarComponent("bugatti")
+export const BMW         = makeCarComponent("bmw")
+export const Mercedes    = makeCarComponent("mercedes")
+export const Ferrari     = makeCarComponent("ferrari")
+export const Audi        = makeCarComponent("audi")
+export const Toyota      = makeCarComponent("toyota")
+export const Volkswagen  = makeCarComponent("volkswagen")
+export const Porsche     = makeCarComponent("porsche")
+export const Lamborghini = makeCarComponent("lamborghini")
+export const Ford        = makeCarComponent("ford")
+export const Chevrolet   = makeCarComponent("chevrolet")
+export const Honda       = makeCarComponent("honda")
+export const Nissan      = makeCarComponent("nissan")
+export const Hyundai     = makeCarComponent("hyundai")
+export const Kia         = makeCarComponent("kia")
+export const Mazda       = makeCarComponent("mazda")
+export const Subaru      = makeCarComponent("subaru")
+export const RollsRoyce  = makeCarComponent("rollsroyce")
+export const Bentley     = makeCarComponent("bentley")
+export const Maserati    = makeCarComponent("maserati")
+export const Jaguar      = makeCarComponent("jaguar")
+export const Volvo       = makeCarComponent("volvo")
+export const Peugeot     = makeCarComponent("peugeot")
+export const Renault     = makeCarComponent("renault")
+export const Tesla       = makeCarComponent("tesla")
+export const Bugatti     = makeCarComponent("bugatti")
 
 export const CAR_COMPONENTS: Record<string, React.ComponentType<{ size?: number }>> = {
   bmw: BMW, mercedes: Mercedes, ferrari: Ferrari, audi: Audi,
